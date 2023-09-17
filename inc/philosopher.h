12992496f9d2b705060844f207eb3d45545bfdaa
dreywesson <dreywesson@gmail.com>
2023-09-17 16:49:19 +0200

    implement validator function

--- inc/philosopher.h
+++ inc/philosopher.h
@@ -6,16 +6,16 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 19:44:36 by doduwole          #+#    #+#             */
-/*   Updated: 2023/09/05 20:32:54 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/09/17 14:32:58 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
 
+# include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <pthread.h>
 
 #endif
--- main.c
+++ main.c
@@ -6,31 +6,65 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 19:37:24 by doduwole          #+#    #+#             */
-/*   Updated: 2023/09/06 16:50:30 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/09/17 16:48:52 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/philosopher.h"
 
-void	*routine(void *arg)
+/**
+ *	@todo
+ * Check inputs and take care of errors
+ */
+
+int	ft_error(char *message)
 {
-	printf("Hello philosopher\n");
-	sleep(3);
-	printf("Dead\n");
-	(void)arg;
-	return (NULL);
+	printf("\033[1;31m"
+			"Error\n"
+			"\033[0m");
+	printf("\033[3m\033[2;37m"
+			" %s\n"
+			"\033[0m",
+			message);
+	return (0);
+}
+
+int	parse_number(char *str)
+{
+	char	tmp;
+
+	tmp = *str;
+	while (*str)
+	{
+		tmp = *str++;
+		if (tmp < 48 || tmp > 57)
+			return (2);
+	}
+	return (0);
+}
+
+void	validate_args(char **argv)
+{
+	while (*argv++)
+	{
+		if (*argv && parse_number(*argv))
+		{
+			ft_error("Invalid argument");
+			break ;
+		}
+	}
+}
+
+int	validator(int argc, char **argv)
+{
+	if (!(argc == 6 || argc == 5))
+		return (ft_error("Too few arguments..."), 1);
+	validate_args(argv);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t t1;
-	pthread_t t2;
-
-	(void)argc;
-	(void)argv;
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_create(&t2, NULL, &routine, NULL);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+	validator(argc, argv);
 	return (0);
 }
